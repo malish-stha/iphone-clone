b2e5384f6164d54f0ed74e2efa44f763791fd3e2
Malish Shrestha <malish.shrestha@deerwalk.edu.np>
2024-07-26 14:22:14 +0545

    feat: Add GSAP animation to Hero component
    The Hero component in `src/components/Hero.jsx` has been updated to include a GSAP animation. This animation fades in the hero title with a delay of 1.5 seconds, improving the visual experience for users.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
